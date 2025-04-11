06d6665afddb1da48733264d4be0d0ba1a6a58f7
theharibalan <srs.haribalan2003@gmail.com>
2025-04-11 17:19:22 +0530

    done with kafka

---
+++ patient-service/src/main/java/org/hbn/patientservice/kafka/KafkaProducer.java
@@ -0,0 +1,18 @@
+package org.hbn.patientservice.kafka;
+
+import org.hbn.patientservice.model.Patient;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+    private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient) {
+
+    }
+}
